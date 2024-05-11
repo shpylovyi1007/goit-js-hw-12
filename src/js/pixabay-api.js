3d76341af177463d7d5d5a7ce4d4e870e7fa362e
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-05-11 15:19:03 +0300

    homework12.update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
+export const perPage = 15;
 
-const userGallery = async userSearch => {
+const userGallery = async (userSearch, myPage) => {
     const response = await axios.get('https://pixabay.com/api/', {
         params: {
             key: '43654443-0aa2ac279d0400a8e119738dd',
             q: userSearch,
             image_type: 'photo',
             orientation: 'horizontal',
-            safesearch: 'true'
+            safesearch: 'true',
+            per_page: perPage,
+            page: myPage
         }
     })
     return response.data;
--- src/main.js
+++ src/main.js
@@ -5,21 +5,58 @@ import addImages from "./js/render-functions";
 const form = document.querySelector('form');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const btn = document.querySelector('.load');
+btn.style.display = 'none';
+import { perPage } from "./js/pixabay-api";
+let myPage = 1;
 
 form.addEventListener('submit', async event => {
     event.preventDefault();
     const userSearch = event.target.elements.search.value.trim();
     loader.style.display = 'flex';
     gallery.innerHTML = '';
-
     try {
-        const response = await userGallery(userSearch);
+        const response = await userGallery(userSearch, myPage);
         if (response.hits.length === 0) {
             iziToast.show({
                 message: 'Sorry, there are no images matching your search query. Please try again!'
             })
         } else {
-            addImages(response);
+            addImages(response)
+            const scroll = () => {
+                const galleryItem = document.querySelector('.gallery-item');
+                const itemHeight = galleryItem.getBoundingClientRect().height;
+                console.log(galleryItem.getBoundingClientRect());
+                window.scrollBy({
+                    top: itemHeight * 2,
+                    behavior: 'smooth'
+                });
+            };
+            scroll();
+
+            btn.style.display = 'flex';
+            const totalPages = Math.ceil(response.totalHits / perPage);
+
+            btn.addEventListener('click', async () => {
+                if (myPage > totalPages) {
+                    return iziToast.error({
+                        position: "topRight",
+                        message: "We're sorry, there are no more posts to load"
+                    });
+                }
+
+                loader.style.display = 'flex';
+                myPage += 1;
+                try {
+                    const response = await userGallery(userSearch, myPage);
+                    addImages(response)
+                    scroll();
+                } catch (error) {
+                    console.log(error);
+                } finally {
+                    loader.style.display = 'none';
+                }
+            });
         }
     } catch (error) {
         console.log(error);
@@ -27,3 +64,5 @@ form.addEventListener('submit', async event => {
         loader.style.display = 'none';
     }
 });
+
+
